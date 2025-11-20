a0af68d170919bb8a0b8443a65450bbb76c2bd0c
Batyrkhan Zhaparov <54882375+batyr-qhan@users.noreply.github.com>
2025-11-20 21:43:18 +0500

    started adding form

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.views.generic.edit import CreateView
 
+from .forms import BbForm
 from .models import Bb, Rubric
 
 
+class BbCreateView(CreateView):
+    template_name = "bboard/create.html"
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
 def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {"bbs": bbs, "rubrics": rubrics}
+    bbs = Bb.objects.order_by("-published")
+    return render(request, "bboard/index.html", context)
+
     bbs = Bb.objects.order_by("-published")
     return render(request, "bboard/index.html", {"bbs": bbs})
 
