4c5b4a389dc70c3dfacd8d3a5cabf8ff03cdc3e6
Batyrkhan Zhaparov <54882375+batyr-qhan@users.noreply.github.com>
2025-11-20 19:33:45 +0500

    Initial commit

---
+++ bboard/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Рубрики"
+        verbose_name = "Рубрика"
+        ordering = ["name"]
+
+
+class Bb(models.Model):
+    rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
+    title = models.CharField(max_length=200, verbose_name="Product")
+    content = models.TextField(null=True, blank=True, verbose_name="Description")
+    price = models.IntegerField(null=True, blank=True, verbose_name="Цена")
+    published = models.DateField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
+
+    class Meta:
+        verbose_name_plural = " Объявления "
+        verbose_name = "Объявления"
+        ordering = ['-published']
